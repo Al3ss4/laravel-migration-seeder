0396680dc423d91e8c97869d7d83ce7e1f8aeea6
Mario Francesco Rovito <alessandro.r14@gmail.com>
2022-05-23 21:39:23 +0200

    add migration

--- database/migrations/2022_05_23_141919_update_trains_table.php
+++ database/migrations/2022_05_23_141919_update_trains_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateUsersTable extends Migration
+class UpdateTrainsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class UpdateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
+        Schema::table('trains', function (Blueprint $table) {
+            $table->date('giorno_di_partenza');
         });
     }
 
@@ -25,8 +25,8 @@ class UpdateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn('giorno_di_partenza');
         });
     }
 }
---
+++ resources/views/train.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.main')
+
+@section('title', 'trains')
+
+@section('trains')
+
+
+                 @dump($trains)
+
+
+
+
+
+
+
+
+
+
+    </main>
+@endsection
